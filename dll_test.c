7243cd91975b10f9f63cc16d35d13b68bd4330b2
jwzhou1} <jwzhou1@github.com>
2023-05-21 21:57:28 +0800

    finish dll_test

--- dll_test.c
+++ dll_test.c
@@ -315,6 +315,219 @@ int unitTest15(int status)
     return passed;
 }
 
+// One test for 'dll_pop_back' fucntion.
+// Here we create an empty DDL to test 'dll_pop_back' fucntion.
+int unitTest16(int status)
+{
+    int passed = 0;
+    dll_t *dll = create_dll();
+    int test = dll_pop_back(dll);
+    if (test == 0)
+    {
+        passed = 1;
+    }
+    free(dll);
+    return passed;
+}
+
+// Another test for 'dll_pop_back' fucntion.
+// Here we create a non-empty DDL to test 'dll_pop_back' fucntion.
+int unitTest17(int status)
+{
+    int passed = 0;
+    dll_t *dll = create_dll();
+    dll_push_front(dll, 5);
+    dll_push_front(dll, 10);
+    dll_push_front(dll, 15);
+    dll_push_front(dll, 20);
+    dll_push_front(dll, 25);
+    int test = dll_pop_back(dll);
+    if (test == 5 && dll->count == 4 && dll->head->data == 25 && dll->tail->data == 10)
+    {
+        passed = 1;
+    }
+    free(dll);
+    return passed;
+}
+
+// One test for 'dll_insert' fucntion.
+// Here we insert a new node at a valid position to test 'dll_insert' fucntion.
+int unitTest18(int status)
+{
+    int passed = 0;
+    dll_t *dll = create_dll();
+    dll_push_back(dll, 5);
+    dll_push_back(dll, 15);
+    dll_push_back(dll, 20);
+    int test = dll_insert(dll, 1, 10);
+    if (test == 1 && dll->count == 4)
+    {
+        node_t *curr = dll->head;
+        int expected[] = {5, 10, 15, 20};
+        for (int i = 0; i < dll->count; i++)
+        {
+            if (curr->data != expected[i])
+            {
+                break;
+            }
+            curr = curr->next;
+        }
+        if (curr == NULL)
+        {
+            passed = 1;
+        }
+    }
+    return passed;
+}
+
+// Another test for 'dll_insert' fucntion.
+// Here we insert a new node at an invalid position to test 'dll_insert' fucntion.
+int unitTest19(int status)
+{
+    int passed = 0;
+    dll_t *dll = create_dll();
+    dll_push_back(dll, 5);
+    dll_push_back(dll, 10);
+    int test = dll_insert(dll, 10, 30);
+    if (test == 0 && dll->count == 2)
+    {
+        passed = 1;
+    }
+    return passed;
+}
+
+// One test for 'dll_get' fucntion.
+// Here we get the item at a valid position to test 'dll_get' fucntion.
+int unitTest20(int status)
+{
+    int passed = 0;
+    dll_t *dll = create_dll();
+    dll_push_back(dll, 5);
+    dll_push_back(dll, 10);
+    dll_push_back(dll, 15);
+    int test = dll_get(dll, 1);
+    if (test == 10)
+    {
+        passed = 1;
+    }
+    return passed;
+}
+
+// Another test for 'dll_get' fucntion.
+// Here we fail to get the item at an invalid position to test 'dll_get' fucntion.
+int unitTest21(int status)
+{
+    int passed = 0;
+    dll_t *dll = create_dll();
+    dll_push_back(dll, 5);
+    dll_push_back(dll, 10);
+    int test = dll_get(dll, 3);
+    if (test == 0)
+    {
+        passed = 1;
+    }
+    return passed;
+}
+
+// One test for 'dll_remove' fucntion.
+// Here we remove the item at a valid position to test 'dll_remove' fucntion.
+int unitTest22(int status)
+{
+    int passed = 0;
+    dll_t *dll = create_dll();
+    dll_push_back(dll, 5);
+    dll_push_back(dll, 10);
+    dll_push_back(dll, 15);
+    int test = dll_remove(dll, 1);
+    if (test == 10 && dll->count == 2 && dll_get(dll, 0) == 5 && dll_get(dll, 1) == 15)
+    {
+        passed = 1;
+    }
+    return passed;
+}
+
+// Another test for 'dll_remove' fucntion.
+// Here we fail to remove the item at an invalid position to test 'dll_remove' fucntion.
+int unitTest23(int status)
+{
+    int passed = 0;
+    dll_t *dll = create_dll();
+    dll_push_back(dll, 5);
+    dll_push_back(dll, 10);
+    int test = dll_remove(dll, 3);
+    if (test == 0 && dll->count == 2)
+    {
+        passed = 1;
+    }
+    return passed;
+}
+
+// One test for 'dll_size' fucntion.
+// Here we create an empty DDL to test 'dll_size' fucntion.
+int unitTest24(int status)
+{
+    int passed = 0;
+    dll_t *dll = create_dll();
+    int test = dll_size(dll);
+    if (test == 0)
+    {
+        passed = 1;
+    }
+    return passed;
+}
+
+// Another test for 'dll_size' fucntion.
+// Here we create a non-empty DDL to test 'dll_size' fucntion.
+int unitTest25(int status)
+{
+    int passed = 0;
+    dll_t *dll = create_dll();
+    dll_push_back(dll, 5);
+    dll_push_back(dll, 10);
+    dll_push_back(dll, 15);
+    dll_push_back(dll, 20);
+    dll_push_back(dll, 25);
+    int result = dll_size(dll);
+    if (result == 5)
+    {
+        passed = 1;
+    }
+    return passed;
+}
+
+// One test for 'free_dll' fucntion.
+// Here we create an empty DDL to test 'free_dll' fucntion.
+int unitTest26(int status)
+{
+    int passed = 0;
+    dll_t *dll = create_dll();
+    free_dll(dll);
+    if (dll != 0)
+    {
+        passed = 1;
+    }
+    return passed;
+}
+
+// Another test for 'free_dll' fucntion.
+// Here we create a non-empty DDL to test 'free_dll' fucntion.
+int unitTest27(int status)
+{
+    int passed = 0;
+    dll_t *dll = create_dll();
+    dll_push_back(dll, 5);
+    dll_push_back(dll, 10);
+    dll_push_back(dll, 15);
+    dll_push_back(dll, 20);
+    dll_push_back(dll, 25);
+    free_dll(dll);
+    if (dll != 5)
+    {
+        passed = 1;
+    }
+    return passed;
+}
+
 // An array of function pointers to all of the tests
 // that main() can use iterate over them.
 // UNCOMMENT Tests as you are ready to use them
@@ -334,7 +547,19 @@ int (*unitTests[])(int) = {
     unitTest12,
     unitTest13,
     unitTest14,
-    unitTest15};
+    unitTest15,
+    unitTest16,
+    unitTest17,
+    unitTest18,
+    unitTest19,
+    unitTest20,
+    unitTest21,
+    unitTest22,
+    unitTest23,
+    unitTest24,
+    unitTest25,
+    unitTest26,
+    unitTest27};
 
 // ====================================================
 // ================== Program Entry ===================
